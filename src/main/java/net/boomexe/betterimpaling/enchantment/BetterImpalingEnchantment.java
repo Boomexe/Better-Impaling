bdf43b3fb3becfefe4e3413a085d72fa50d84b80
AlexBurneikis <alexburneikis@gmail.com>
2023-01-12 18:38:24 +1100

    Fixed enchanting

--- src/main/java/net/boomexe/betterimpaling/enchantment/BetterImpalingEnchantment.java
+++ src/main/java/net/boomexe/betterimpaling/enchantment/BetterImpalingEnchantment.java
@@ -13,4 +13,16 @@ public class BetterImpalingEnchantment extends Enchantment {
     public float getAttackDamage(int level, Entity group) {
         return 0.0f;
     }
+
+    public int getMinPower(int level) {
+        return 1 + (level - 1) * 8;
+    }
+
+    public int getMaxPower(int level) {
+        return this.getMinPower(level) + 20;
+    }
+
+    public int getMaxLevel() {
+        return 5;
+    }
 }
